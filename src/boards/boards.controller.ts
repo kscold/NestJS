53dccb477fa413003617ab441e9d106ab4165499
KIM SEUNG CHAN <201921339@sangmyung.kr>
2024-09-08 00:23:22 +0900

    NestJS ORM을 통한 모든 CRUD 로직 완성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.entity';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { BoardStatus } from './board-status.enum';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,8 +15,24 @@ export class BoardsController {
         return this.boardsService.createBoard(createBoardDto);
     }
 
+    @Get('/')
+    getAllBoards(): Promise<Board[]> {
+        return this.boardsService.getAllBoards();
+    }
+
     @Get('/:id')
     getBoardById(@Param('id') id: number): Promise<Board> {
         return this.boardsService.getBoardById(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(@Param('id', ParseIntPipe) id: number, @Body('status', BoardStatusValidationPipe) status: BoardStatus): Promise<Board> {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
+
+    @Delete('/:id')
+    // url 파라미터가 숫자로 제대로 오는지 숫자로 오지 않으면 ParseIntPipe를 통해서 에러를 일으킴
+    deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
+        return this.boardsService.deleteBoard(id);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -16,6 +16,10 @@ export class BoardsService {
         return this.boardRepository.createBoard(createBoardDto);
     }
 
+    async getAllBoards(): Promise<Board[]> {
+        return await this.boardRepository.find();
+    }
+
     async getBoardById(id: number): Promise<Board> {
         const found = await this.boardRepository.findOne({ where: { id } });
 
@@ -23,5 +27,24 @@ export class BoardsService {
             throw new NotFoundException(`Can't find Board with id ${id}`);
         }
         return found;
+    }
+
+    async deleteBoard(id: number): Promise<void> {
+        const result = await this.boardRepository.delete(id);
+
+        // 삭제를 못했을 시에는 affected === 0이므로 NotFoundException 404 오류를 반환
+        if (result.affected === 0) {
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+        console.log('result', result);
+    }
+
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        const board = await this.getBoardById(id);
+
+        board.status = status;
+        await this.boardRepository.save(board);
+
+        return board;
     }
 }
