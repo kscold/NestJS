fe2f0ea3483e16d652124ad881ac0eef92a9c2b2
KIM SEUNG CHAN <201921339@sangmyung.kr>
2024-09-09 00:55:49 +0900

    NestJS 기본 auth 개발

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfing } from './configs/typeorm.confing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeORMConfing), // TypeORM 설정
-        BoardsModule,
+        BoardsModule, AuthModule,
     ],
 })
 export class AppModule {}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,26 @@
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+    constructor(dataSource: DataSource) {
+        super(User, dataSource.createEntityManager());
+    }
+
+    async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+        const user = this.create({ username, password });
+
+        try {
+            await this.save(user);
+        } catch (error) {
+            if ((error.code = '23505')) {
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+}
