1871c18c3a84e6b99c42dfb7329b98c36543c206
KIM SEUNG CHAN <201921339@sangmyung.kr>
2024-10-13 00:53:15 +0900

    NestJS 영화 api 개발 초기 구조

---
+++ netflix/src/movie/movie.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+export interface Movie {
+    id: number;
+    title: string;
+    genre: string;
+}
+
+@Injectable()
+export class MovieService {
+    private movies: Movie[] = [
+        {
+            id: 1,
+            title: '해리포터',
+            genre: 'fantasy',
+        },
+        {
+            id: 2,
+            title: '반지의 제왕',
+            genre: 'action',
+        },
+    ];
+    private idCounter = 3;
+
+    getManyMovies(title?: string) {
+        if (!title) {
+            return this.movies;
+        }
+        return this.movies.filter((m) => m.title.startsWith(title));
+    }
+
+    getMovieById(id: number) {
+        const movie = this.movies.find((m) => m.id === +id);
+        if (!movie) {
+            throw new NotFoundException('존재하지 않는 ID의 영화입니다.');
+        }
+
+        return movie;
+    }
+
+    createMovie(createMovieDto: CreateMovieDto) {
+        const movie: Movie = {
+            id: this.idCounter++,
+            ...createMovieDto,
+        };
+
+        this.movies.push(movie);
+
+        return movie;
+    }
+
+    updateMovie(id: number, updateMovieDto: UpdateMovieDto) {
+        const movie = this.movies.find((m) => m.id === +id);
+
+        if (!movie) {
+            throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
+        }
+
+        Object.assign(movie, updateMovieDto);
+
+        return movie;
+    }
+
+    deleteMovie(id: number) {}
+}
