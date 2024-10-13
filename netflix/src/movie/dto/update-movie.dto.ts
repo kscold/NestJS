cb0fc17b59c5d39817e3f3213e8b7657d68c5529
KIM SEUNG CHAN <201921339@sangmyung.kr>
2024-10-13 23:44:42 +0900

    NestJS class-vaildate 복습

--- netflix/src/movie/dto/update-movie.dto.ts
+++ netflix/src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,15 @@
+import { IsDefined, IsNotEmpty, IsOptional } from 'class-validator';
+
 export class UpdateMovieDto {
+    @IsNotEmpty() // 값이 있는 경우에만 수정가능
+    @IsOptional()
     title?: string;
+
+    @IsNotEmpty()
+    @IsOptional()
     genre?: string;
+
+    // null | undefind 이 아니면 가능
+    @IsDefined()
+    test: string;
 }
